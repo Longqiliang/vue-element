15ef301504fe12c5a84d36ebd0811c872be863bc
lql <821599372@qq.com>
2018-07-16 18:35:45 +0800

    table component update

--- src/views/table/community/index.vue
+++ src/views/table/community/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <el-form :model="form" ref="communityForm" >
+  <el-form ref="communityForm" >
     <el-row type="flex" class="form-row">
       <el-col :span="8">
         <div class="input-group field-search">
@@ -25,7 +25,7 @@
       <el-col :span="8">
         <div class="input-group field-search">
           <label for="s" class="input-group__label">小区名称</label>
-          <el-input v-model="form.name" placeholder="请输入小区名称"></el-input>
+          <el-input placeholder="请输入小区名称"></el-input>
         </div>
       </el-col>
     </el-row>
@@ -53,7 +53,7 @@
       <el-col :span="8">
         <div class="input-group field-search">
           <label for="s" class="input-group__label">小区名称</label>
-          <el-input v-model="form.name" placeholder="请输入小区名称"></el-input>
+          <el-input placeholder="请输入小区名称"></el-input>
         </div>
       </el-col>
     </el-row>
@@ -63,127 +63,131 @@
     </div>
   </el-form>
     
-  <div>
-    <div class="table-header">
-      <h4 class="table-title">小区列表</h4>
-      <div class="table-title-slot">
-        <el-button type="warning" size="small">导出</el-button>
-        <el-button type="primary" size="small">新增</el-button>
-        <el-button type="danger" size="small">删除</el-button>
-      </div>
-    </div>
-    <el-table :data="table" style="width: 100%" border header-cell-class-name="default-bg">
-      <el-table-column v-for="(col, c) in columns" :prop="col.name || col.label" :key="c" :label="col.label" :min-width="col.width" :align="col.align || align" :type="col.type">
-          <template v-if="col.children">
-              <el-table-column v-for="(ch,o) in col.children" :key="o" :label="ch.label" :prop="ch.name || ch.label"></el-table-column>
-          </template>
-          <template v-else-if="col.slot"  slot-scope="scope">
-              {{ scope.row.name }}
-          </template>
-      </el-table-column>  
-    </el-table>
-    <el-pagination :total="total" :current-page="listQuery.pageIndex" :page-size="listQuery.pageSize" layout="prev, pager, next, ->, jumper, slot, total" @current-change="handleCurrentChange" class="pagination">
-      <slot>
-        <span class="pagination__count">{{pageCount}}</span>
-      </slot>
-    </el-pagination>
-  </div>  
-    
+   <BTable v-bind="table" @handleSelectionChange="handleSelectionChange"  @handleCurrentChange="handleCurrentChange" />
+  <BTable v-bind="table" @handleSelectionChange="handleSelectionChange"  @handleCurrentChange="handleCurrentChange" />
 </div>
 
 </template>
 
 <script>
+import BTable from '@/components/Table'
 export default {
+  components:{
+    BTable
+  },
   data() {
     return {
-      table: [
-        {
-          index: "1",
-          name: "丽华佳园",
-          street: "福永街道",
-          station: "富华工作站",
-          houseWork: "物业服务企业",
-          person: "李安",
-          phone: "136-2342-2462",
-          completeness: "20%"
-        }
-      ],
-      align: "center",
-      isMultiple: true,
-      tableHeader: [
-        {
-          label: "序号",
-          name: "index",
-          width: 50
+      table: {
+        list: [
+          {
+            index: "1",
+            name: "丽华佳园",
+            street: "福永街道",
+            station: "富华工作站",
+            houseWork: "物业服务企业",
+            person: "李安",
+            phone: "136-2342-2462",
+            completeness: "20%"
+          }
+        ],
+        isMultiple: true,
+        tableHeader: [
+          {
+            label: "序号",
+            name: "index",
+            width: 50
+          },
+          {
+            label: "小区名称",
+            name: "name",
+            width: 80
+          },
+          {
+            label: "所属街道办",
+            name: "street",
+            width: 90
+          },
+          {
+            label: "所属工作站",
+            name: "station",
+            width: 90
+          },
+          {
+            label: "物业服务企业",
+            name: "houseWork",
+            width: 160
+          },
+          {
+            label: "物业经理",
+            name: "person",
+            width: 80
+          },
+          {
+            label: "联系方式",
+            name: "phone",
+            width: 140
+          },
+          {
+            label: "信息完成度",
+            name: "completeness",
+            width: 100
+          }
+        ],
+        listQuery: {
+          pageIndex: 1,
+          pageSize: 10
         },
-        {
-          label: "小区名称",
-          name: "name",
-          width: 80
+        total: 50,
+        operates: {
+          show: true,
+          list: [
+            {
+              label: '查看详情',
+              type: 'text',
+              method: (index, row) => {
+                this.handleDelete(index, row)
+              }
+            }
+          ]
         },
-        {
-          label: "所属街道办",
-          name: "street",
-          width: 90
+        title: {
+          show: true,
+          align: 'left',
+          label: '小区列表',
+          list: [
+            {
+              label: '删除',
+              type: 'danger',
+              native: true,
+              method:(index, row) => {
+                this.handleSelectionRemove()
+              } 
+            }
+          ]
         },
-        {
-          label: "所属工作站",
-          name: "station",
-          width: 90
-        },
-        {
-          label: "物业服务企业",
-          name: "houseWork",
-          width: 160
-        },
-        {
-          label: "物业经理",
-          name: "person",
-          width: 80
-        },
-        {
-          label: "联系方式",
-          name: "phone",
-          width: 140
-        },
-        {
-          label: "信息完成度",
-          name: "completeness",
-          width: 100
-        },
-        {
-          label: "操作",
-          name: "查看详情",
-          width: 100
-        }
-      ],
-      listQuery: {
-        pageIndex: 1,
-        pageSize: 10
+        multipleSelection: [] //多行选中
       },
-      total: 50,
-      form:{}
+      
     }
-  },
-  computed:{
-      columns() {
-        if(this.isMultiple){
-            this.tableHeader.unshift({
-                type: 'selection',
-                width: 50
-            })   
-        }
-        return this.tableHeader 
-      },
-      pageCount() {
-        let count =  Math.ceil(this.total/this.listQuery.pageSize)
-        return `共${count}页、`
-      }
   },
   methods: {
     handleCurrentChange(val) {
-      this.listQuery.pageIndex = val
+      this.table.listQuery.pageIndex = val
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    },
+    handleSelectionChange(val) {
+      this.table.multipleSelection = val
+    },
+    handleSelectionRemove(val) {
+      this.$confirm('此操作将删除所选列表中的信息, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        console.log(this.table.multipleSelection)
+      })
     }
   }
 };
